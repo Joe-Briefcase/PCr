a903c0f182a1d092bbf062eb3926bc25017e9189
Joe-Briefcase <s185143@student.dtu.dk>
2019-11-04 10:22:48 +0100

    Initial commit

---
+++ app/src/main/java/dk/dtu/philipsclockradio/StateRadio.java
@@ -0,0 +1,11 @@
+package dk.dtu.philipsclockradio;
+
+public class StateRadio extends StateAdapter {
+
+    // Skifter mellem FM og AM.
+    @Override
+    public void onClick_Power(ContextClockradio context) {
+
+    }
+
+}
